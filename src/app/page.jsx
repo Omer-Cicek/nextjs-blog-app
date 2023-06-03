14a9f09b05bbd95f9a3188f065326eae4a1ade6a
Omer-Cicek <omercicek160408@gmail.com>
2023-06-03 03:17:59 +0300

    about page created

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
+import Button from '@/components/Button/Button'
 
 export default function Home() {
     return (
@@ -10,7 +11,7 @@ export default function Home() {
                 <p className={styles.explanation}>
                     Turning your idea into reality. We bring together the teams from the global tech industry.
                 </p>
-                <button className={styles.button}>See our works</button>
+                <Button url="/portfolio" text="See our works" />
             </div>
             <div className={styles.item}>
                 <Image src= {Hero} alt='hero' className={styles.img}/>
